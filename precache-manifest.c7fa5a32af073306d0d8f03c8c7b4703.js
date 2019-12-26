366d8ba4f554780a58d5ecebc3d52ec4fa6cbb12
Mikhail Katsupko <t63723@gmail.com>
2019-12-26 16:29:20 +0700

    Updates

---
+++ precache-manifest.c7fa5a32af073306d0d8f03c8c7b4703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf8ddd712061f7491139a7d46e4b63d",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "196f1a4d5147a345e857",
+    "url": "/vk-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "00297afab9ecda8fc5d1",
+    "url": "/vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "196f1a4d5147a345e857",
+    "url": "/vk-app/static/js/2.8cf8f62d.chunk.js"
+  },
+  {
+    "revision": "00297afab9ecda8fc5d1",
+    "url": "/vk-app/static/js/main.aca30e72.chunk.js"
+  },
+  {
+    "revision": "0c7594eca5acad1a7634",
+    "url": "/vk-app/static/js/runtime-main.29aeed70.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
